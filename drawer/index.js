8d08f7218fde66ceb5d57b58d372944a4941fba2
safwen-code <safwendjebbi1234@gmail.com>
2022-03-25 11:44:37 +0100

    new modification in drawer

--- drawer/index.js
+++ drawer/index.js
@@ -1,13 +1,46 @@
-$(() => {
-  console.log('hi')
+$(function () {
+  $('#view').load('./acceuil.html')
 
-  const drawer = $('#drawer').dxDrawer({
-    // template is a propritée specifies the drawer's content
+  $('#drawer')
+    .dxDrawer({
+      template: function (e) {
+        return $("<div style='width: 150px'>Drawer content</div>")
+      },
+      height: 250,
+      minSize: 37,
 
-    template: function (e) {
-      return $("<div style='width: 150px'>Drawer content</div>")
-    },
-    height: 250,
-    minSize: 37,
+      //navigate to pages with dxlist
+      template: function (e) {
+        var $list = $('<div/>').dxList({
+          items: [
+            { id: 1, text: 'acceuil', icon: 'message', filePath: 'acceuil' },
+            { id: 2, text: 'login', icon: 'check', filePath: 'login' },
+            { id: 3, text: 'register', icon: 'trash', filePath: 'register' },
+          ],
+          width: 200,
+          selectionMode: 'single',
+          onSelectionChanged: function (e) {
+            $('#view').load(e.addedItems[0].filePath + '.html')
+          },
+        })
+        return $list
+      },
+    })
+    .dxDrawer('instance')
+
+  //open and close drawer
+  $('#toolbar').dxToolbar({
+    items: [
+      {
+        widget: 'dxButton',
+        location: 'before',
+        options: {
+          icon: 'menu',
+          onClick: function () {
+            drawer.toggle()
+          },
+        },
+      },
+    ],
   })
 })
